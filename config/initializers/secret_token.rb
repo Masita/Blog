f3f5bc80d18b7f3d67ee208c65c2deeb2894fb7a
Jeremy Lefevre <lefevre.baes.jeremy@gmail.com>
2013-10-24 14:12:12 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasitaBlog::Application.config.secret_token = '5fc10e36960639c11796605f34f094b7ff68c2d01725f648f58f1adee98f9fddf21f5311b2769018a7a2d7910e643035ef563dc5abec871a50234ea181a61ee3'
